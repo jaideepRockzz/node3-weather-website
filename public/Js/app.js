ca551b9f6097b24da0e7a1ad631bdcca6a96a8b6
unknown <m.jaideepu@gmail.com>
2022-03-09 09:44:31 +0530

    weather application issue fix one

--- public/Js/app.js
+++ public/Js/app.js
@@ -1,24 +1,3 @@
-console.log('java script loadedd!')
-
-// fetch('https://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data.puzzle)
-//     })
-// })
-
-fetch('http://localhost:2000/weather?address=!').then((response) => {
-    response.json().then((dataweather) => {
-        console.log(dataweather.error)
-        // if(dataweather.error){
-        // console.log(dataweather.error)
-        // }else{
-        // console.log(dataweather.forecast)
-        // console.log(dataweather.location)
-        // console.log(dataweather.address)
-        // }
-    })
-})
-
 const weatherform = document.querySelector('form')
 const inputtext = document.querySelector('input')
 const messageone = document.querySelector('#message1')
